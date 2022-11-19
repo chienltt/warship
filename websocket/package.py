46d391aef41388427eb3a523ea79d6b42758b3aa
dvdat <d.v.dat160901@gmail.com>
2022-11-19 20:48:35 +0700

    define package

--- websocket/package.py
+++ websocket/package.py
@@ -1,3 +1,7 @@
+import json
+
+from helper import convertStrToInt
+
 ngrok_ip = "0.tcp.ap.ngrok.io"
 ngrok_port = 17618
 game_name = "warship"
@@ -5,20 +9,96 @@ game_des = "none"
 game_author = "wda"
 championship_server_host = "104.194.240.16"
 championship_server_port = 8881
-
-PKG_HELLO = (1).to_bytes(4, 'little') + (len(ngrok_ip)).to_bytes(4, 'little')
-+ bytes(ngrok_ip, 'utf-8') + ngrok_port.to_bytes(4, 'little') + (
-    len(game_name)).to_bytes(4, 'little')
-+ bytes(game_name, 'utf-8') + (
-    len(game_des)).to_bytes(4, 'little')
-+ bytes(game_des, 'utf-8') + (
-    len(game_author)).to_bytes(4, 'little')
-+ bytes(game_author, 'utf-8')
-
-
-def get_pkg_hello(data):
-    return {
-        "abc"
+    
+# 1: pkt_start
+def pkt_start(uuid, op_uuid, match_id):
+    data = {
+        "type": 1,
+        "uuid": uuid,
+        "op_uuild": op_uuid,
+        "match_id": match_id
     }
+    return json.dumps(data) 
 
+# 2: pkt_ok
+# Return string
+def pkt_ok():
+    data = {
+        "type": 2
+    }
+    return json.dumps(data)
 
+# 4: pkt_your_turn
+def pkt_your_turn(number_of_bullets):
+    data = {
+        "type": 4,
+        "number_of_bullets": number_of_bullets
+    }
+    return json.dumps(data)
+
+# 6: pkt_enemy_fired
+# coordinates = [{"x": x1, "y": y1}, {"x": x2, "y": y2},...]
+def pkt_enemy_fired(number_of_shots, coordinates):
+    data = {
+        "type": 6,
+        "number_of_shots": number_of_shots,
+        "coordinates": coordinates
+    }
+    return json.dumps(data)
+
+# 7: pkt_destroyed
+# coordinates = [
+#  {
+#   "head": {x: x1, y: y1}, 
+#   "tail": {x: x2, y: y2}
+#  },
+#  {
+#   "head": {x: x3, y: y3}, 
+#   "tail": {x: x4, y: y4}
+#  },
+# ]
+def pkt_destroyed(destroyed_ships, coordinates):
+    data = {
+        "type": 6,
+        "destroyed_ships": destroyed_ships,
+        "coordinates": coordinates
+    }
+    return json.dumps(data)
+
+# 8: pkt_victory
+def pkt_victory(uuid):
+    data = {
+        "type": 8,
+        "uuid": uuid
+    }
+    return json.dumps(data)
+
+# 9: pkt_defeat
+def pkt_defeat(uuid):
+    data = {
+        "type": 8,
+        "uuid": uuid
+    }
+    return json.dumps(data)
+
+# 10: pkt_wait
+def pkt_wait():
+    data = {
+        "type": 10
+    }
+    return json.dumps(data)
+
+# 11: pkt_error
+def pkt_error(error):
+    data = {
+        "type": 11,
+        "error": error
+    }
+    return json.dumps(data)
+
+# 12: pkt_bye
+def pkt_bye():
+    data = {
+        "type": 12
+    }
+    return json.dumps(data)
--- websocket/web_server.py
+++ websocket/web_server.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 
 import websockets
 
@@ -10,26 +11,26 @@ game_author = "wda"
 championship_server_host = "104.194.240.16"
 championship_server_port = 8881
 
+def pkt_start(uuid, op_uuid, match_id):
+    data = {
+        "type": 1,
+        "uuid": uuid,
+        "op_uuild": op_uuid,
+        "match_id": match_id
+    }
+    return json.dumps(data)
 
 # create handler for each connection
 
 async def handler(websocket, path):
     while True:
         data = await websocket.recv()
-        reply = f"Data recieved as:  {data}!"
-        print("okok", reply)
-        print("okok1", type(reply))
-        # await websocket.send(
-        #     (1).to_bytes(4, 'little') + (len(ngrok_ip)).to_bytes(4, 'little')
-        #     + bytes(ngrok_ip, 'utf-8') + ngrok_port.to_bytes(4, 'little') + (
-        #         len(game_name)).to_bytes(4, 'little')
-        #     + bytes(game_name, 'utf-8') + (
-        #         len(game_des)).to_bytes(4, 'little')
-        #     + bytes(game_des, 'utf-8') + (
-        #         len(game_author)).to_bytes(4, 'little')
-        #     + bytes(game_author, 'utf-8'))
-        await websocket.send("1/40/24/chien dat hoang "
-                             "hung/658/45/awdawdawdawdawd")
+        print("hello pkt", data)
+        print("type pkt hello", type(data))
+        loadData = json.loads(data)
+        print("loadData", loadData)
+        print("typeLoadData", type(loadData))
+        await websocket.send(pkt_start(loadData.get("uuid"), "op_Uuid", loadData.get("match_id")))
 
 
 start_server = websockets.serve(handler, "localhost", 8000)
